f5fd84e90826fd491f993b8652d9a6f7bb72c16c
chickenfried42 <alexsitu928@gmail.com>
2026-03-07 12:39:15 -0800

    Update 1
    added some stuff

--- chat/public/script.js
+++ chat/public/script.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
 import { updateProfile, getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { getFirestore, collection, addDoc, getDocs, onSnapshot, serverTimestamp, doc, query, orderBy, limit } from 'https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js'
-import { messageElement } from "./stuff.js";
+import { changeTheme, manualElement, messageElement, scrollToBottom } from "./stuff.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAJCNAJjc3IEGKEtpdZ59QrsabG2IH_yc8",
@@ -22,6 +22,7 @@ const db = getFirestore(app);
 let user;
 let displayName;
 let snapshotListener;
+let room = "chat1";
 
 auth.onAuthStateChanged(function(u) {
   if(u) {
@@ -62,7 +63,21 @@ document.addEventListener("keydown", async function(event) {
 
 function sendMessage(text) {
 //  if(!text.length || text.length > 256) return;
-  if(text.length > 256) return;
+  if(text.length > 256) return alert("message too long (must be < 256 characters)");
+
+  if(text.startsWith("/room")) {
+    const arg = text.split(" ")[1];
+    if(!arg) {
+      alert("select a room to switch to");
+      return;
+    }
+    if(arg.length>64) return alert("room name too long");
+    room = arg;
+    document.getElementById("messagebox").placeholder = `message '${arg}'`;
+    receiveChat();
+    return;
+  }
+
   const message = {
     content: text,
     author: {
@@ -70,12 +85,12 @@ function sendMessage(text) {
       id: user.uid
     },
   }
-  //messageElement(message);
+  //messageElement(message
   writeMessage(message);
 }
 
 async function writeMessage(message) {
-  const doc = await addDoc(collection(db, "chats", "main", "messages"), {
+  const doc = await addDoc(collection(db, "chats", room, "messages"), {
     content: message.content,
     author: {
       name: message.author.name,
@@ -89,16 +104,24 @@ async function writeMessage(message) {
 
 async function receiveChat() {
   if(snapshotListener) snapshotListener(); 
-  //i always found removing the listener by calling it as a function seemed weird, something like snapshotListener.unsubscribe() would be more intuitive but what can I say I'm not google and I'm sure they had their reasons.
-  const q = query(collection(db, "chats", "main", "messages"), orderBy("created_at", "desc"), limit(32));
+  const q = query(collection(db, "chats", room, "messages"), orderBy("created_at", "desc"), limit(32));
   let firstTime = true;
 
   snapshotListener = onSnapshot(q, async(snapshot) => {
     if(firstTime) {
+      document.querySelector(".chatbox").innerHTML = "";
+      messageElement({
+        content: `room has been changed to ${room}`,
+        author: {
+          name: "???",
+          id:"67!!!!",
+        }
+      })
       const messages = snapshot.docs.map(m => m.data()).reverse();
       messages.forEach(message => {
         messageElement(message);
       })
+      scrollToBottom(true);
       firstTime = false;
       return;
     }
@@ -107,9 +130,39 @@ async function receiveChat() {
         const message = change.doc.data();
         // if(message.author.id == user.uid) return;
         messageElement(message);
+        scrollToBottom();
       }
-    })
+    });
   })
 }
 
 receiveChat();
+
+document.querySelector("#logout").addEventListener("click", async function() {
+  signOut(auth).then(() => {
+    window.location.reload();
+  }).catch((err) => {
+    alert(err); //hope nobody ever gets this error
+  }) 
+});
+
+const themeButtons = document.querySelectorAll(".themeswitcher");
+themeButtons.forEach(button => { 
+  console.log(button);
+  button.addEventListener("click", function(element) {
+    let color = button.id; //red, blue, green, random??
+    console.log("button");
+    changeTheme(color);
+  })
+});
+
+
+manualElement("/room <name>", "changes the active chat room");
+  document.querySelector("#manualtoggle").addEventListener("click", function() {
+  const manual = document.querySelector(".manual");
+  if(manual.classList.contains("invis")) {
+    manual.classList.remove("invis");
+  } else {
+    manual.classList.add("invis");
+  }
+});
--- chat/public/stuff.js
+++ chat/public/stuff.js
@@ -1,18 +1,102 @@
-const message = {
-  content: "ahahahhheahhahahaha",
-  author: "uid of user"
+const themes = {
+  "red": {
+    chatboxColor: "rgb(54, 54, 54)",
+    borderColor: "rgb(223, 72, 61)",
+    nameColor: "rgb(223, 72, 61)",
+    messageHover: "rgb(63, 63, 63)",
+    chatboxText: "white",
+    logoutColor: "white",
+    alt: "green",
+    buttonColor: "white"
+  },
+  "green": {
+    chatboxColor: "rgb(54, 54, 54)",
+    borderColor: "rgb(50, 238, 144)",
+    nameColor: "rgb(50, 238, 144)",
+    messageHover: "rgb(63, 63, 63)",
+    chatboxText: "white",
+    logoutColor: "rgb(255, 79, 79)",
+    alt: "green",
+    buttonColor: "rgb(50, 238, 144)",
+  },
+  "blue": {
+    chatboxColor: "#e3eff6",
+    borderColor: "#39ace7",
+    nameColor: "rgb(38, 46, 90)",
+    messageHover: "#b7e5ff83",
+    chatboxText: "rgb(53, 63, 122)",
+    logoutColor: "rgb(53, 63, 122)",
+    alt: "green",
+    buttonColor: "#39ace7"
+  },
+  "brown": {
+    chatboxColor: "rgb(229,222,207)",
+    borderColor: "rgb(217,189,165)",
+    nameColor: "rgb(38, 46, 90)",
+    messageHover: "#b7e5ff83",
+    chatboxText: "rgb(53, 63, 122)",
+    logoutColor: "rgb(53, 63, 122)",
+    alt: "green",
+    buttonColor: "rgb(217,189,165)"
+  }
 }
-
-// here i decided to use jquery for some reason (i've barely used it before)
-
+// behold the one instance of jquery usage in this entire project
 export function messageElement(message) {
+  let date;
+  if(message.created_at) {
+      date = message.created_at.toDate();
+  } else {
+      date = new Date();
+  }
+
+  let timeInfo = `${date.toLocaleDateString("en-US", {dateStyle: "short"})} ${date.toLocaleTimeString("en-US", {timeStyle:"short"})}`;
+  timeInfo = timeInfo.replace(`${new Date().toLocaleDateString("en-US", {dateStyle: "short"})} `, "")
+
+  
   const $box = $('<div>').addClass('message');
   const $author = $('<span>').addClass('author').text(message.author.name+": ");
   const $content = $('<span>').text(message.content);
+  const $timestamp = $('<span>').text(" - " + timeInfo);
   $box.append($author);
   $box.append($content);
+  $box.append($timestamp);
   $('.chatbox').append($box);
   return $box;
 }
 
-// however integrating jquery with my other code is kinda hard for a neophyte like me so I'm just going to leave it in this module
+export function manualElement(command, description) {
+  const $box = $('<div>').addClass('cmd');
+  const $name = $('<span>').addClass('author').text(command+" - ");
+  const $desc = $('<span>').text(description);
+  $box.append($name);
+  $box.append($desc);
+  $('.manual').append($box);
+  return $box;
+}
+
+
+export function changeTheme(t) {
+  const theme = themes[t]
+  const root = document.documentElement;
+  root.style.setProperty('--chatbox-color', theme.chatboxColor);
+  root.style.setProperty('--border-color', theme.borderColor);
+  root.style.setProperty("--namecolor", theme.nameColor);
+  root.style.setProperty("--message-hover", theme.messageHover);
+  root.style.setProperty("--chatbox-text", theme.chatboxText);
+  root.style.setProperty("--logout-color", theme.logoutColor);
+  root.style.setProperty("--alt", theme.altColor);
+  root.style.setProperty("--button-color", theme.buttonColor); 
+
+  // root.style.setProperty('--border-color', theme.chatboxColor);
+}
+
+export function scrollToBottom(f) {
+  const chatbox = document.querySelector(".chatbox");
+  if(chatbox.scrollHeight-chatbox.scrollTop < 400 || !f) {
+    console.log(chatbox.scrollHeight);
+    console.log(chatbox.scrollTop);
+    return;
+  }
+  chatbox.scrollTop = chatbox.scrollHeight;
+}
+
